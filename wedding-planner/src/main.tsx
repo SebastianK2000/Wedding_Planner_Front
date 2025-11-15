2d9b95f823258142d12018d209c83f38a069286e
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-11-15 20:09:34 +0100

    update css files

--- wedding-planner/src/main.tsx
+++ wedding-planner/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import './index.css'
 import App from './App'
